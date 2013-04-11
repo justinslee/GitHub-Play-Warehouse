b7cf1eb242eebdb42e064e7234b2c7d5e0b660cd
Justin Lee <justinlee808@hotmail.com>
2013-04-10 20:58:20 -1000

    Initial Commit

---
+++ app/models/Tag.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import play.db.ebean.Model;
+
+@Entity 
+public class Tag extends Model {
+  private static final long serialVersionUID = -5950706943271500599L;
+  @Id
+  public Long id;
+  public String name;
+  @ManyToMany(mappedBy="tags", cascade=CascadeType.ALL)
+  public List<Product> products = new ArrayList<>();
+  
+  public Tag(String name) {
+    this.name = name;
+  }
+  
+  public static Finder<Long,Tag> find() {
+    return new Finder<Long, Tag>(Long.class, Tag.class);
+  }
+}
